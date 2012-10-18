9068cbb13176cb0baf98768d0be9753286212165
Hector Castro <hectcastro@gmail.com>
2012-10-18 13:49:14 -0400

    v0.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,3 +5,7 @@
 ## v0.1.1
 
 * Added explicit disabling of Apache `000-default` site.
+
+## v0.1.2
+
+* Replaced reference to API endpoint with localhost.
--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Hector Castro"
 maintainer_email  "hectcastro@gmail.com"
 license           "Apache 2.0"
 description       "Installs and configures Uptime."
-version           "0.1.1"
+version           "0.1.2"
 recipe            "uptime", "Installs and configures Uptime"
 
 %w{ apache2 git logrotate mongodb nodejs }.each do |d|
